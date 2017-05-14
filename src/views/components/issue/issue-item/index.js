54f59970fe3cd466a1e0bfb590660a9099fc1eaa
Lucas Sacramento <lucas.arki@gmail.com>
2017-05-14 13:41:09 +0100

    Installed jpeg loader and cleaned some stuff

--- src/views/components/issue/issue-item/index.js
+++ src/views/components/issue/issue-item/index.js
@@ -1,12 +1,7 @@
-import classNames from 'classnames';
 import React, {Component, PropTypes} from 'react';
 import {Issue} from 'src/core/issues';
-import IssuePage from '../issue-page';
 import {Link} from 'react-router';
-import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
-
-import appicon from '../../../../images/app-store-icon.png';
+import appIcon from '../../../../images/app-store-icon.png';
 
 class IssueItem extends Component {
 
@@ -14,7 +9,6 @@ class IssueItem extends Component {
     super(props, context);
 
     this.handleIssueClick = ::this.handleIssueClick;
-
   }
 
   renderTitle(issue) {
@@ -29,40 +23,27 @@ class IssueItem extends Component {
     );
   }
 
-
-  renderIssuePage(issue) {
-    return (
-      <IssuePage
-        title={issue}
-      />
-    );
-  }
-
   handleIssueClick() {
-    // console.log(this.props.issue.key)
     this.props.onClick(this.props.issue.key);
   }
 
   render() {
-
     const {issue} = this.props;
-    console.log(issue.key)
-
     return (
       <Link activeClassName="active" to={{pathname: '/issues/page', query: {id: `${issue.key}`}}}>
-      <div className="issue-item">
-        <div className="issue-item-left">
+        <div className="issue-item">
+          <div className="issue-item-left">
           <span className="issue-item-left--bodyText">
             {issue.body}
           </span>
-          <span className="issue-item-left--user">
+            <span className="issue-item-left--user">
             {issue.user.displayName}
           </span>
+          </div>
+          <div className="issue-item-right">
+            <img src={appIcon} alt="Icon"/>
+          </div>
         </div>
-        <div className="issue-item-right">
-          <img src={appicon} alt="Icon" />
-        </div>
-      </div>
       </Link>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ const PORT = 3000;
 const loaders = {
   js: {test: /\.js$/, exclude: /node_modules/, loader: 'babel'},
   scss: {test: /\.scss$/, loader: 'style!css!postcss!sass'},
-  img:{test: /\.(jpe?g|png|gif|svg)$/i, loaders: [ 'file?hash=sha512&digest=hex&name=[hash].[ext]', 'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false']}
+  img: {test: /\.(jpe?g|png|gif|svg)$/i, loaders: [ 'file?hash=sha512&digest=hex&name=[hash].[ext]', 'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false']}
 };
 
 
